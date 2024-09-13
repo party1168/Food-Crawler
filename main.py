74547a890572070e81b646bd5ecfb2f3508a2124
ljhvvs1181 <a2775269@gmail.com>
2024-09-13 18:16:06 +0800

    改善效率 改成multi-threading

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # main.py
+import concurrent.futures
 from tqdm import tqdm
 from scrapers.africanbites_scraper import AfricanBitesScraper
 from scrapers.tastyoven_scraper import TastyOvenScraper
@@ -7,7 +8,7 @@ from scrapers.feelgoodfoodie_scraper import FeelGoodFoodieScraper
 from utils.data_utils import save_to_csv, save_to_json
 
 def scrap_website(scraper):
-    print(f"正在爬取 {scraper.scraper.__class__.__name__}")
+    print(f"正在爬取 {scraper.__class__.__name__}")
     recipes = scraper.scrape_all_recipes()
     return recipes
 def main():
@@ -21,22 +22,25 @@ def main():
     fieldname = ['recipe_name','ingredients','url']
     filename = 'recipes.csv'
     all_recipes = []
-    for scraper in tqdm(scrapers,desc="爬取網站進度",unit="scraper"):
-        print(f"正在爬取 {scraper.__class__.__name__}")
-        recipes = scraper.scrape_all_recipes()
-        all_recipes.extend(recipes)
-        # 打印結果
-        for index, recipe in enumerate(recipes, 1):
-            print(f"\n--- Recipe {index} ---")
-            print(f"Name: {recipe['recipe_name']}")
-            print(f"URL: {recipe['url']}")
-            print("Ingredients:")
-            for ingredient in recipe['ingredients']:
-                print(f"  - {ingredient}")
-            print("-" * 30)
-
-        print(f"\nTotal {len(recipes)} recipes")
-
+    with concurrent.futures.ThreadPoolExecutor(max_workers=len(scrapers)) as executor:
+        future_to_scraper = {executor.submit(scrap_website,scraper): scraper for scraper in scrapers}
+        for future in tqdm(concurrent.futures.as_completed(future_to_scraper),total=len(scrapers),desc="Scrape Website Progress",unit="website"):
+            scraper = future_to_scraper[future]
+            try:
+                recipes = future.result()
+                all_recipes.extend(recipes)
+                # 打印結果
+                for index, recipe in enumerate(recipes, 1):
+                    print(f"\n--- Recipe {index} ---")
+                    print(f"Name: {recipe['recipe_name']}")
+                    print(f"URL: {recipe['url']}")
+                    print("Ingredients:")
+                    for ingredient in recipe['ingredients']:
+                        print(f"  - {ingredient}")
+                    print("-" * 30)
+                print(f"\nTotal {len(recipes)} recipes for {scraper.__class__.__name__}")
+            except Exception as e:
+                print(f"{scraper.__class__.__name__} generated an exception: {e}")
         # 保存結果
     save_to_csv(all_recipes, filename=filename,fieldnames=fieldname,mode="w")
 
